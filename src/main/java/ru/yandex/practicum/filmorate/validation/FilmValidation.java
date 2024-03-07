659f522dc32f0fd3bd2c0f2d04dfe573e7ac517a
Viktor Nemov <vik.nemov.vas@yandex.ru>
2024-03-08 00:19:28 +0300

    fix(sprint9): правки форматирования

--- src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
@@ -11,6 +11,7 @@ import java.time.Month;
 public class FilmValidation {
     private static final Integer MAX_LENGTH_DESCRIPTION = 200;
     private static final LocalDate BIRTHDAY_MOVIE = LocalDate.of(1895, Month.DECEMBER, 28);
+
     public static void validation(Film film) throws ValidationFilmException {
         if (film.getName() == null || film.getName().isBlank()) {
             throw new ValidationFilmException("Название фильма не может быть путым!");
