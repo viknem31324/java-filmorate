94a4caf33557e3b2cb9a71903d810f676a97813a
Viktor Nemov <vik.nemov.vas@yandex.ru>
2024-03-18 23:47:30 +0300

    feat(sprint10): дорабатывает обработку ошибок

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -4,12 +4,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.ExceptionResponse;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 
 @RestControllerAdvice
 public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleIncorrectParameterException(final ParamsIncorrectException e) {
+        return new ExceptionResponse(
+                String.format("Ошибка с полем \"%s\".", e.getParameter())
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleValidationFilmException(final ValidationFilmException e) {
+        return new ExceptionResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleValidationUserException(final ValidationUserException e) {
+        return new ExceptionResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handleInvalidEmailException(final UserAlreadyExistException e) {
+        return new ExceptionResponse(e.getMessage());
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ExceptionResponse handleUserNotFoundException(final UserNotFoundException e) {
@@ -20,5 +45,11 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ExceptionResponse handleFilmNotFoundException(final FilmNotFoundException e) {
         return new ExceptionResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handleThrowable(final Throwable e) {
+        return new ExceptionResponse("Произошла непредвиденная ошибка.");
     }
 }
