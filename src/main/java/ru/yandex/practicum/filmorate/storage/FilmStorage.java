3a38ebbaa239e1d0348dd9304e7de7281c4fa9d3
Viktor Nemov <vik.nemov.vas@yandex.ru>
2024-03-20 22:45:08 +0300

    fix(sprint10): дорабатывает код после проверки линтера

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,10 +6,16 @@ import java.util.List;
 
 public interface FilmStorage {
     List<Film> findAllFilms();
+
     Film findFilmById(int filmId);
+
     Film createFilm(Film requestFilm);
+
     Film updateFilm(Film film);
+
     Film addToLikes(long filmId, long userId);
+
     Film deleteFromLikes(long filmId, long userId);
+
     List<Film> findPopularFilms(int count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,11 +6,18 @@ import java.util.List;
 
 public interface UserStorage {
     List<User> findAllUsers();
+
     User findUserById(long userId);
+
     User createUser(User requestUser);
+
     User updateUser(User requestUser);
+
     User addToFriends(long id, long friendId);
+
     User deleteFromFriends(long id, long friendId);
+
     List<User> findUserFriends(long id);
+
     List<User> findMutualFriends(long id, long otherId);
 }
