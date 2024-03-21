eae0f9e757c5e24bb9c70e430710081fe1f0455a
Viktor Nemov <vik.nemov.vas@yandex.ru>
2024-03-22 00:05:10 +0300

    fix(sprint10): правки валидатора

--- src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidationTest.java
@@ -19,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class UserValidationTest {
     @Autowired
     private UserService service;
+
     @Test
     public void checkValidationEmail() {
         User user = User.builder()
